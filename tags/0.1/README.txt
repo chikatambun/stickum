b9e2f93a15f7f8e47c3ecdcef8833ba087bad280
spleeman <spleeman@a5f907cf-6820-0410-bc37-230e6677d226>
2006-10-23 12:57:43 +0000

    Tagging the 0.1 release

---
+++ tags/0.1/README.txt
@@ -0,0 +1,6 @@
+stickum
+
+This is a TurboGears (http://www.turbogears.org) project. It can be
+started by running the start-stickum.py script.
+
+URL: http://code.google.com/p/stickum
---
+++ tags/0.1/stickum/model.py
@@ -0,0 +1,75 @@
+from sqlalchemy import *
+from sqlalchemy.ext.assignmapper import assign_mapper
+
+from turbogears.database import metadata, session
+
+from datetime import datetime
+
+from stickum.util import Parser
+
+from cStringIO import StringIO
+
+from stickum.silvercity import html_splitlines, SilverCityRenderer
+
+paste_table = Table("pastes", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("pasted_on", DateTime, default=datetime.now()),
+    Column("lang", String(50), default="Python"),
+    Column("content", Unicode),
+    Column("formatted_content", Unicode)
+)
+
+setting_table = Table("settings", metadata,
+    Column("name", String(50), primary_key=True),
+    Column("data", PickleType)
+)
+
+class Paste(object):
+    
+    @classmethod
+    def get_latest(self):
+        limit = Setting.get("latest_limit").data
+        if not limit:
+            # Set a reasonable default
+            limit = 10
+            s = Setting()
+            s.name = "latest_limit"
+            s.data = limit
+            s.flush()
+        return Paste.select(order_by=desc("pasted_on"), limit=limit)
+        
+    @classmethod
+    def get_count(self):
+        return select([func.count(paste_table.c.id)]).scalar()
+        
+    def _get_formatted_content(self):
+        if not self._formatted_content:
+            if not self.lang == "None":                
+                self._formatted_content = SilverCityRenderer().render(self.lang, self.content)
+                self.flush()
+            else:
+                return self.content
+                self._formatted_content = self.content
+                self.flush()
+        return self._formatted_content
+
+        
+    def _set_formatted_content(self, content):
+        self._formatted_content = content
+    
+    formatted_content = property(_get_formatted_content, _set_formatted_content)
+    
+    def _get_formatted_lines(self):
+        return html_splitlines(self.formatted_content.splitlines())
+    formatted_lines = property(_get_formatted_lines)
+        
+    
+class Setting(object):
+    pass
+    
+assign_mapper(session.context, Paste, paste_table,
+    properties={
+        "_formatted_content": paste_table.c.formatted_content
+    }
+)
+assign_mapper(session.context, Setting, setting_table)
